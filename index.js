9adfde16004bea0f0e18c52d0d5df2a08530cb38
Salena-1 <116763048+Salena-1@users.noreply.github.com>
2023-06-22 14:24:53 -0300

    i

--- index.js
+++
@@ -1,6 +0,0 @@
-let email= ''
-email= document.getElementById("email").value 
-localStorage.setItem("email", email);
-localStorage.getItem("email");
-localStorage.clear();
-localStorage.removeItem("email"); 
