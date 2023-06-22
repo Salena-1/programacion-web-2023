e7618a4236ab2b66de7b7da888107e16b6fc2558
Salena-1 <116763048+Salena-1@users.noreply.github.com>
2023-06-22 16:41:29 -0300

    f

--- catalogo.js
+++ catalogo.js
@@ -1,4 +1,14 @@
-// traigo variables desde el html
+
+let productos=[
+    {id:1, nombre: "Collar Aria", cantidad: 1,descripcion: "Collar combinado con pendiente de caracol y perlas.", precio:352, img:`images\collar1.jpg`},
+//     faltan agregar los demás productos//
+]
+
+
+
+
+// traigo variables desde el html y las inicializo para poder usarlas
+
 const informacionCompra = document.getElementById('informacionCompra');
 const contenedorCompra = document.getElementById('contenedorCompra');
 const productosCompra = document.getElementById('productosCompra');
@@ -10,8 +20,33 @@ const total = document.getElementById('total');
 const body = document.querySelector("body");
 const x = document.getElementById('x')
 
+
+
+
+
+
+
 let lista = []
 let valortotal = 0
+
+
+// me falta ver cómo hacer para "conectar" el producto en la pagina, con el boton de comprar y que aparezca acá
+
+
+
+
+
+function visualizarProductos() {
+    grid.innerHTML = ""
+    for (let i = 0; i < productos.length; i++) {
+        grid.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
+    }
+}
+
+
+
+
+
 
 function comprar(indice) {
     lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
@@ -20,33 +55,42 @@ function comprar(indice) {
     return lista
 }
 
+carrito.addEventListener("click", function(){
+    body.style.overflow = "hidden"
+    contenedorCompra.classList.remove('none')
+    contenedorCompra.classList.add('contenedorCompra')
+    informacionCompra.classList.add('informacionCompra')
+    mostrarElemtrosLista()
+})
+
+
+
 function mostrarElemtrosLista() {
     productosCompra.innerHTML = ""
     valortotal = 0
     for (let i = 0; i < lista.length; i++){
         productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`
-        valortotal += parseInt(lista[i].precio)
+        valortotal += parseInt(lista[i].precio)   // parseINt es para que sea un número entero
     }
     total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal}</span></p>`
 }
 
 function eliminar(indice){
-    let van = true
-    let i = 0
-    while (van == true) {
-        if (productos[i].nombre == lista[indice].nombre) {
-            productos[i].existencia += 1
-            lista.splice(indice, 1)
-            van = false
-        }
-        i += 1
-    }
-    guardarAlmacenamientoLocal("productos", productos)
+    (productos[i].nombre == lista[indice].nombre) 
+    lista.splice(indice, 1)
 
     numero.innerHTML = lista.length
     if (lista.length == 0){
         numero.classList.remove("diseñoNumero")
     }
-    visualizarProductos()
+    // visualizarProductos()
     mostrarElemtrosLista()
 }
+
+
+x.addEventListener("click", function(){
+    body.style.overflow = "auto"
+    contenedorCompra.classList.add('none')
+    contenedorCompra.classList.remove('contenedorCompra')
+    informacionCompra.classList.remove('informacionCompra')
+})
