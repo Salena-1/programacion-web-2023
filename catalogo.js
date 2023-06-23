005627338d6e458b1fb9475cbece45941ec81cbc
Salena-1 <116763048+Salena-1@users.noreply.github.com>
2023-06-22 22:55:09 -0300

    ñl

--- catalogo.js
+++ catalogo.js
@@ -1,96 +1,227 @@
 
-let productos=[
-    {id:1, nombre: "Collar Aria", cantidad: 1,descripcion: "Collar combinado con pendiente de caracol y perlas.", precio:352, img:`images\collar1.jpg`},
-//     faltan agregar los demás productos//
-]
+let carrito = [];  // Variable para almacenar los productos seleccionados
 
+function añadirAlCarrito(marcador) {        // Función para añadir un producto al carrito
+  
+  let producto = obtenerProductoPorMarcador(marcador);   // Obtener la información del producto seleccionado
+  
+  carrito.push(producto);   // Añadir el producto al carrito
+  
+  mostrarCarrito(); // Actualizar el carrito en el DOM
+}
 
+// Función para obtener la información de un producto por su marcador
+function obtenerProductoPorMarcador(marcador) {
+  let productos = obtenerProductos();
+  return productos.find(producto => producto.marcador === marcador);
+}
 
-
-// traigo variables desde el html y las inicializo para poder usarlas
-
-const informacionCompra = document.getElementById('informacionCompra');
-const contenedorCompra = document.getElementById('contenedorCompra');
-const productosCompra = document.getElementById('productosCompra');
-const grid = document.getElementById('grid');
-const carrito = document.getElementById('carrito');
-const numero = document.getElementById("numero");
-const header = document.querySelector("#header");
-const total = document.getElementById('total');
-const body = document.querySelector("body");
-const x = document.getElementById('x')
-
-
-
-
-
-
-
-let lista = []
-let valortotal = 0
-
-
-// me falta ver cómo hacer para "conectar" el producto en la pagina, con el boton de comprar y que aparezca acá
-
-
-
-
-
-function visualizarProductos() {
-    grid.innerHTML = ""
-    for (let i = 0; i < productos.length; i++) {
-        grid.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
+// Función para obtener todos los productos disponibles
+function obtenerProductos() {
+  return [
+    {
+      marcador: 1,
+      titulo: "Collar Aria",
+      precio: 352
+    },
+    {
+      marcador: 2,
+      titulo: "Collar Nara",
+      precio: 251
+    },
+    {
+      marcador: 3,
+      titulo: "Collar Lisa",
+      precio: 411
+    },
+    {
+        marcador:4,
+        titulo:"Anillo Megara",
+        precio:369
+    },
+    {
+        marcador:5,
+        titulo:"Anillo Soia",
+        precio:621
+    },
+    {
+        marcador:6,
+        titulo:"Anillo Idra",
+        precio:357
+    },
+    {
+        marcador:7,
+        titulo:"Aros Corallo",
+        precio:502
+    },
+    {
+        marcador:8,
+        titulo:"Aros Pranna",
+        precio:432
+    },
+    {
+        marcador:9,
+        titulo:"Aros Formentera",
+        precio:468
+    },
+    {
+      marcador:10,
+      titulo:"Collar Aida",
+      precio:1280
+    },
+    {
+      marcador:11,
+      titulo:"Aros Jarara",
+      precio:630
+    },
+    {
+      marcador:12,
+      titulo:"Set anillos Aqua",
+      precio:3680
     }
+  ];
+}
+
+
+
+// Event listener para los botones "Añadir al Carrito"
+let botonesCarrito = document.querySelectorAll(".btn-primary[marcador]");
+botonesCarrito.forEach(boton => {
+  boton.addEventListener("click", function() {
+    let marcador = parseInt(this.getAttribute("marcador"));
+    añadirAlCarrito(marcador);
+  });
+});
+
+// // Llamar a la función mostrarCarrito inicialmente para mostrar el carrito vacío
+// mostrarCarrito();
+
+function añadirAlCarrito(marcador) {
+  // Obtener la información del producto seleccionado
+  let producto = obtenerProductoPorMarcador(marcador);
+  
+  // Verificar si el producto ya está en el carrito
+  let productoExistente = carrito.find(item => item.marcador === marcador);
+  
+  if (productoExistente) {
+    // Si el producto ya existe, aumentar el contador
+    productoExistente.cantidad++;
+  } else {
+    // Si el producto no existe, agregarlo al carrito con contador inicial de 1
+    producto.cantidad = 1;
+    carrito.push(producto);
+  }
+  
+  // Actualizar el carrito en el DOM
+  mostrarCarrito();
+}
+
+function mostrarCarrito() {
+  let carritoElement = document.getElementById("compras");
+  carritoElement.innerHTML = ""; // Limpiar el contenido del carrito
+
+  let subtotal = 0;
+
+  if (carrito.length === 0) {
+    carritoElement.innerHTML = "<p>No hay productos en el carrito.</p>";
+  } else {
+    carrito.forEach((producto, index) => {
+      const compraElement = document.createElement("div");
+      compraElement.classList.add("compra");
+
+      const tituloElement = document.createElement("p");
+      tituloElement.classList.add("titulo");
+      tituloElement.textContent = `${producto.titulo} - Precio: $${producto.precio} - Cantidad: ${producto.cantidad}`;
+      compraElement.appendChild(tituloElement);
+
+      const eliminarElement = document.createElement("span");
+      eliminarElement.classList.add("eliminar");
+      eliminarElement.textContent = "x";
+      eliminarElement.addEventListener("click", () => eliminarDelCarrito(index));
+      compraElement.appendChild(eliminarElement);
+
+      carritoElement.appendChild(compraElement);
+
+      subtotal += producto.precio * producto.cantidad;
+
+
+      // Ocultar el mensaje de éxito
+      const mensajeExito = document.getElementById('mensajeExito');
+      mensajeExito.style.display = 'none';
+    });
+  }
+
+  let subtotalElement = document.getElementById("subtotalcompra");
+  subtotalElement.textContent = `Subtotal: $${subtotal}`;
+}
+
+function eliminarDelCarrito(index) {
+  const producto = carrito[index];
+  producto.cantidad--; // Reducir la cantidad en 1
+
+  if (producto.cantidad <= 0) {
+    carrito.splice(index, 1); // Si la cantidad es menor o igual a 0, eliminar el elemento del carrito
+  }
+
+  mostrarCarrito(); // Actualizar el carrito en el DOM
+}
+
+function finalizarCompra() {
+  
+  if (carrito.length === 0) {
+    alert("No hay items en el carrito.");
+    return; 
+  }
+
+ 
+  var modalCompra = new bootstrap.Modal(document.getElementById('modalCompra'));
+  modalCompra.show();
+}
+
+function reiniciarCarrito() {
+  carrito = []; 
+  mostrarCarrito(); 
+}
+
+function completarCompra() {
+  
+  var nombre = document.getElementById('nombre').value;
+  var apellido = document.getElementById('apellido').value;
+  var edad = document.getElementById('edad').value;
+  var numeroTarjeta = document.getElementById('numeroTarjeta').value;
+
+
+  if (nombre.trim() === '' || apellido.trim() === '' || isNaN(edad) || numeroTarjeta.length !== 16) {
+    alert("Por favor, complete todos los campos correctamente.");
+    return; 
+  }
+
+
+  if (parseInt(edad) < 18) {
+    alert("Debe ser mayor de 18 años para realizar la compra.");
+    return; 
+  }
+
+ 
+  var modalCompra = new bootstrap.Modal(document.getElementById('modalCompra'));
+  modalCompra.hide();
+
+ 
+  alert("Su compra se ha realizado con éxito.");
+
+
+  reiniciarCarrito();
 }
 
 
 
 
+var botonVaciarCarrito = document.querySelector(".vaciar-carrito");
+botonVaciarCarrito.addEventListener("click", vaciarCarrito);
 
 
-function comprar(indice) {
-    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
-    numero.innerHTML = lista.length
-    numero.classList.add("diseñoNumero")
-    return lista
+function vaciarCarrito() {
+  carrito = []; 
+
+  mostrarCarrito();
 }
-
-carrito.addEventListener("click", function(){
-    body.style.overflow = "hidden"
-    contenedorCompra.classList.remove('none')
-    contenedorCompra.classList.add('contenedorCompra')
-    informacionCompra.classList.add('informacionCompra')
-    mostrarElemtrosLista()
-})
-
-
-
-function mostrarElemtrosLista() {
-    productosCompra.innerHTML = ""
-    valortotal = 0
-    for (let i = 0; i < lista.length; i++){
-        productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`
-        valortotal += parseInt(lista[i].precio)   // parseINt es para que sea un número entero
-    }
-    total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal}</span></p>`
-}
-
-function eliminar(indice){
-    (productos[i].nombre == lista[indice].nombre) 
-    lista.splice(indice, 1)
-
-    numero.innerHTML = lista.length
-    if (lista.length == 0){
-        numero.classList.remove("diseñoNumero")
-    }
-    // visualizarProductos()
-    mostrarElemtrosLista()
-}
-
-
-x.addEventListener("click", function(){
-    body.style.overflow = "auto"
-    contenedorCompra.classList.add('none')
-    contenedorCompra.classList.remove('contenedorCompra')
-    informacionCompra.classList.remove('informacionCompra')
-})
